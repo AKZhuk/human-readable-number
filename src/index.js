077ebc388291896e8a3edc2061a28235cb10b373
github-AKZhuk <lesha.zhuk@gmail.com>
2021-02-19 19:24:45 +0300

    feat: inplement tast

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let arrOfNumber = String(number).split("");
+    let readable = "";
+    let isOne = false;
+    let isZero = false;
+    numStr = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+    };
+    let tentoStr = {
+        2: "twenty",
+        3: "thirty",
+        4: "forty",
+        5: "fifty",
+        6: "sixty",
+        7: "seventy",
+        8: "eighty",
+        9: "ninety",
+    };
+    if (number <= 20) {
+        readable = readable + numStr[number];
+    } else if (99 < number < 1000) {
+        for (let i = 0; i < arrOfNumber.length; i++) {
+            if (i == 0) {
+                readable = numStr[arrOfNumber[i]] + " hundred ";
+            } else if (i == 1) {
+                switch (arrOfNumber[i]) {
+                    case "0":
+                        isZero = true;
+                        readable = readable + numStr[arrOfNumber[i + 1]];
+                        break;
+                    case "1":
+                        isOne = true;
+                        readable;
+                        break;
+                    default:
+                        readable = readable + tentoStr[arrOfNumber[i]];
+                        break;
+                }
+            } else if (i == 2) {
+                if (isOne == true) {
+                    readable = readable + numStr[Number(arrOfNumber[i]) + 10];
+                } else if (isZero == true) {
+                    readable;
+                } else {
+                    arrOfNumber[i] != 0 && isOne !== true
+                        ? (readable = readable + " " + numStr[arrOfNumber[i]])
+                        : readable;
+                }
+            }
+        }
+    }
+
+    return readable;
+};
